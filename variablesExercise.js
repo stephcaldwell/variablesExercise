ab29f7707e7ba6dce63c2e2986259fa9f1d5e98c
stephcaldwell <stephcaldwell97@gmail.com>
2021-02-23 20:12:57 -0600

    Corrected Exercise

--- variablesExercise.js
+++ variablesExercise.js
@@ -1,27 +1,29 @@
 //1
 //The variable type to use when the value will not change is the const variable
 //2
-let num = 20
+let num = 20;
 //3
-num = 15
+num = 15;
 //4
-num = num+7
+num = num+7;
 //5
-num--
+num--;
 //6
-num ="this is not a number"
+num ="this is not a number";
 //7
-let blank = undefined
-//9
-blank=null
+// let blank = undefined;
+let blank;
 //8
+console.log(blank);
+//9
+blank=null;
 console.log(blank);
 //Bonus
 //1
-const quarter=0.25, dime=0.1, nickel = 0.05, penny = 0.01
+const quarter=0.25, dime=0.1, nickel = 0.05, penny = 0.01;
 //2
 console.log(quarter, dime, nickel, penny);
 //3
-let letters ="qwertyuiop"
+let letters ="qwertyuiop";
 //4
-console.log(letters [9]);
+console.log(letters [letters .length -1]);
