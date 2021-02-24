a6bcf7d69ef929c0b66f83bb4eb5906a8d4b80b7
stephcaldwell <stephcaldwell97@gmail.com>
2021-02-23 19:56:05 -0600

    Exercise with Bonuses

---
+++ variablesExercise.js
@@ -0,0 +1,27 @@
+//1
+//The variable type to use when the value will not change is the const variable
+//2
+let num = 20
+//3
+num = 15
+//4
+num = num+7
+//5
+num--
+//6
+num ="this is not a number"
+//7
+let blank = undefined
+//9
+blank=null
+//8
+console.log(blank);
+//Bonus
+//1
+const quarter=0.25, dime=0.1, nickel = 0.05, penny = 0.01
+//2
+console.log(quarter, dime, nickel, penny);
+//3
+let letters ="qwertyuiop"
+//4
+console.log(letters [9]);
